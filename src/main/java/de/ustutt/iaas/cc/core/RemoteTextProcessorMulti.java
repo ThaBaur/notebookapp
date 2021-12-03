7a1adcb6044e1e043f288a1d07bacb1d88260d34
Tobias Baur <baur.tobias@outlook.com>
2021-12-03 20:06:23 +0100

    implement task 01

--- src/main/java/de/ustutt/iaas/cc/core/RemoteTextProcessorMulti.java
+++ src/main/java/de/ustutt/iaas/cc/core/RemoteTextProcessorMulti.java
@@ -1,8 +1,16 @@
 package de.ustutt.iaas.cc.core;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A text processor that sends the text to one of a set of remote REST API for
@@ -13,17 +21,24 @@ import javax.ws.rs.client.Client;
  */
 public class RemoteTextProcessorMulti implements ITextProcessor {
 
+	private final static Logger logger = LoggerFactory.getLogger(RemoteTextProcessorMulti.class);
+
+	private List<WebTarget> targets =  new LinkedList<WebTarget>();
+	AtomicInteger ai = new AtomicInteger();
+
 	public RemoteTextProcessorMulti(List<String> textProcessorResources, Client client) {
 		super();
-		// TODO ...
+		for (String processor: textProcessorResources) {
+			this.targets.add(client.target(processor));
+		}
 	}
 
 	@Override
 	public String process(String text) {
-		// text processing placeholder, to be replaced by your solution :-)
-		String processedText = "[processed by incomplete RemoteTextProcessorMulti] - " + text;
+		int next = ai.getAndIncrement() % targets.size();
+		logger.debug("Selecting endpoint at index {}.", next);
 
-		// TODO send request to "next" text processor endpoint (following some load balancing strategy)
+		String processedText = targets.get(next).request(MediaType.TEXT_PLAIN).post(Entity.entity(text, MediaType.TEXT_PLAIN), String.class);
 
 		return processedText;
 	}
